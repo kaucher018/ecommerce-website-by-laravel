e23c730bafb6b803363741147453170ad547e948
The Kaucher <muhammadkaucher018@gmail.com>
2025-06-23 17:21:21 +0600

    contact blade

--- website/public/contact.blade.php
+++ website/public/contact.blade.php
@@ -36,7 +36,7 @@
       <nav class="navbar navbar-expand-lg custom_nav-container ">
         <a class="navbar-brand" href="index.html">
           <span>
-            Giftos
+            Logeachi.com
           </span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,42 +46,70 @@
         <div class="collapse navbar-collapse innerpage_navbar" id="navbarSupportedContent">
           <ul class="navbar-nav  ">
             <li class="nav-item ">
-              <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{url('/')}}">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="shop.html">
+               <a class="nav-link" href="{{ url('shop') }}">
                 Shop
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="why.html">
+              <a class="nav-link" href="{{ url('why') }}">
                 Why Us
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="testimonial.html">
+              <a class="nav-link" href="{{ url('testimonial') }}">
                 Testimonial
               </a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="contact.html">Contact Us</a>
+              <a class="nav-link" href="{{ url('contact') }}">Contact Us</a>
             </li>
           </ul>
-          <div class="user_option">
-            <a href="">
-              <i class="fa fa-user" aria-hidden="true"></i>
-              <span>
-                Login
-              </span>
+
+
+        <div class="user_option">
+ @if (Route::has('login'))
+          @auth
+
+          <a href="{{url('mycart')}}">
+              <i class="fa fa-shopping-bag" aria-hidden="true"></i>{{$count}}
             </a>
-            <a href="">
-              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+            <a href="{{url('myorder')}}">
+              Myorder 
             </a>
             <form class="form-inline ">
               <button class="btn nav_search-btn" type="submit">
                 <i class="fa fa-search" aria-hidden="true"></i>
               </button>
-            </form>
+              </form>
+
+
+            <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <input type="submit" value="Logout" class="btn btn-success" padding="5px" style="margin-left: 20%;">
+                </form>
+
+                @else
+
+            <a href="{{url('/login')}}">
+              <i class="fa fa-user" aria-hidden="true"></i>
+              <span>
+                Login
+              </span>
+            </a>
+
+            <a href="{{url('/register')}}">
+              <i class="fa fa vcard" aria-hidden="true"></i>
+              <span>
+              register
+              </span>
+            </a>
+
+            @endif
+            @endauth       
           </div>
         </div>
       </nav>
